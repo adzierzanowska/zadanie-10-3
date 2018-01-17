2170e0078ac72818cc131e810d614560508e6dd4
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-01-17 15:37:00 +0100

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 $(function(){
     var carouselList = $("#carousel ul");
+    setInterval(changeSlide, 3000);
 });
 
 setInterval(changeSlide, 3000) {
@@ -11,17 +12,18 @@ setInterval(changeSlide, 3000) {
     }
 }
 
-// function changeSlide() {
-//   $carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
 
-// }
+function changeSlide() {
+    $carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+}
 
-// function moveFirstSlide() {
-//    var firstItem = carouselList.find("li:first");
-//    var lastItem = carouselList.find("li:last");
-//    lastItem.after(firstItem)
-//    carouselList.css({marginLeft:0});
-//}
+
+function moveFirstSlide() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem)
+    carouselList.css({marginLeft:0});
+}
 
 // To samo co wyzej ale pierwsza wersja zapisu //
 /*
